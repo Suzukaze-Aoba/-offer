fe29c2c317e8d8b27f3c998e6e09c19fb9f03a24
Suzukaze-Aoba <814683072@qq.com>
2019-07-12 21:15:56 +0800

    Middle

---
+++ leetcode/Unique Binary Search Trees.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numTrees(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return n;
+        }
+        int *dp = new int[n + 1];
+        dp[0] = dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; ++i) {
+            dp[i] = 0;
+            for (int j = 0; j < i; ++j) {
+                dp[i] += dp[j] * dp[i - j - 1];
+            }
+        }
+        return dp[n];
+    }
+};
